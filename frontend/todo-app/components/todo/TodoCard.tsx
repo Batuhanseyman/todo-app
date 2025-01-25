1c12fd9f5772d0e70094b813444c827fda04f77d
Batuhan SEYMAN <batuhan.seyman@outlook.com>
2025-01-25 20:21:24 +0300

    css change

--- frontend/todo-app/components/todo/TodoCard.tsx
+++ frontend/todo-app/components/todo/TodoCard.tsx
@@ -16,12 +16,12 @@ const toDos = [
 const TodoCard = () => {
   return (
 
-<div className="bg-white  shadow-lg  border-solid rounded-3xl p-16 w-10/12 md:w-9/12 xl:w-5/12 flex flex-col lg:mt-24 items-center content-center">
+<div className="bg-white  shadow-lg  border-solid rounded-3xl p-16  flex flex-col lg:mt-24 items-center content-center">
     <h1 className="text-3xl font-bold text-center
     text-gray-900 mb-6">Todo List</h1>
     <div className="mb-4 flex">
         <Input type="text" placeholder="Add a new todo"
-            className= "!px-3 !py-2 !border !rounded-l-2xl !focus:outline-none !focus:ring-2 !focus:ring-blue-500 !text-gray-900 !text-sm !md:text-xl"></Input>
+            className= ""></Input>
 
         <Button className="bg-blue-500 text-white
             px-4 py-2 rounded-r-2xl hover:bg-blue-600">Add</Button>
