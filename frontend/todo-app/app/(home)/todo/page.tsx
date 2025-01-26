8acb1046ef70366b433caae802dec1accfbaa6ca
Batuhan SEYMAN <batuhan.seyman@outlook.com>
2025-01-26 18:44:17 +0300

    Update todo page.tsx
    An attempt was made to log out with the sign out button. This button and the logout process will be moved to the navbar.

--- frontend/todo-app/app/(home)/todo/page.tsx
+++ frontend/todo-app/app/(home)/todo/page.tsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import {Button} from '@/components/ui/button'
 import TodoCard from '@/components/todo/TodoCard'
+import { useRouter } from 'next/router'
+import { logoutUser } from '@/firebase/firebaseAuthService'
 
 const page = () => {
+  const router = useRouter();
+
+  const handleSignOut = async () => {
+    try {
+      await logoutUser(); 
+      console.log("Kullanıcı çıkışı başarılı.");
+      router.push("/login"); 
+    } catch (error) {
+      console.error("Çıkış hatası:", error);
+    }
+  };
+
+
   return (
     <div className='min-h-screen  w-full flex flex-col items-center  bg-gradient-to-r from-blue-600 to-emerald-400'>
       <div className="self-end mb-4 mt-3">
         <Button className="border rounded-lg bg-red-600 text-center p-2 
-         text-white hover:bg-red-700">
+         text-white hover:bg-red-700" onClick={handleSignOut}>
           Sign Out
          </Button>        
       </div>
