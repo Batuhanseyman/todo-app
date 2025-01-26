63756320c3a66618b7df4da02b827877f30ca5c3
Batuhan SEYMAN <batuhan.seyman@outlook.com>
2025-01-26 23:36:07 +0300

    Update Navbar component
    Navbar component is updated using useAuth custom hook.

--- frontend/todo-app/components/layout/Navbar.tsx
+++ frontend/todo-app/components/layout/Navbar.tsx
@@ -4,11 +4,13 @@ import React, { useState} from 'react'
 import { Button } from '../ui/button'
 import { logoutUser } from '@/firebase/firebaseAuthService'
 import { useRouter } from 'next/navigation';
+import useAuth from '@/hooks/useAuth'
 
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const router = useRouter();
+  const {user, loading} = useAuth();
 
   const handleSignOut = async () => {
     try {
@@ -29,10 +31,18 @@ const Navbar = () => {
     <div className='container mx-auto flex justify-between items-center'>
       <Link className='text-lg font-bold' href='/'>TODO APP</Link>
       <div className= {`${isOpen ? 'flex' : 'hidden'} flex-col md:flex-row md:block`}>
-        <Link href={'/login'} className='mx-2 hover:text-gray-300'>Sign In</Link>
-        <Link href={'/register'} className='mx-2 hover:text-gray-300'>Sign up</Link>
-        <Button className="border rounded-lg bg-red-600 text-center p-2 
-         text-white hover:bg-red-700" onClick={handleSignOut}>Sign Out</Button>
+        {user ? (
+          <>
+            <Button className="border rounded-lg bg-red-600 text-center p-2 
+          text-white hover:bg-red-700" onClick={handleSignOut}>Sign Out</Button>
+          </>          
+        ) : (
+          <>
+          <Link href={'/login'} className='mx-2 hover:text-gray-300'>Sign In</Link>
+          <Link href={'/register'} className='mx-2 hover:text-gray-300'>Sign up</Link>
+          </>
+
+        )}
       </div>
       <div className='md:hidden flex items-center'>
         <Button onClick={() => {
