f3ac02004437d4a37624de958eed0c5fb7ddcb86
Batuhan SEYMAN <batuhan.seyman@outlook.com>
2025-01-27 20:05:58 +0300

    Update Register page.tsx
    Added go to home link

--- frontend/todo-app/app/(auth)/register/page.tsx
+++ frontend/todo-app/app/(auth)/register/page.tsx
@@ -1,11 +1,17 @@
 import RegisterForm from '@/components/layout/RegisterForm'
 import React from 'react'
+import Link from 'next/link'
 
 const Page = () => {
   return (
+    <>
+      <div className='bg-gradient-to-r from-blue-600 to-emerald-400 relative'>
+        <Link href = '/' className='absolute left-4 top-2  bg-gray-950 text-white border rounded-lg p-2'>🡸 Go to home page</Link>
+      </div>
     <div className='flex justify-center items-center min-h-screen bg-gradient-to-r from-blue-600 to-emerald-400 w-full'>
       <RegisterForm></RegisterForm>
     </div>
+    </>
   )
 }
 
