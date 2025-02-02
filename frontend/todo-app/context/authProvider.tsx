9dbe5b45ecc5a4168a21d028b53a3f99eaaf1bc2
Batuhan SEYMAN <batuhan.seyman@outlook.com>
2025-02-02 20:22:41 +0300

    provider implemented

---
+++ frontend/todo-app/context/authProvider.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { createContext, useState, useEffect, ReactNode } from "react";
+import { User, onAuthStateChanged, getAuth  } from "firebase/auth";
+import {auth} from "@/firebase/firebaseConfig"
+import { setSessionCookie, getSessionCookie, removeSessionCookie } from "@/lib/cookies";
+
+interface AuthContextType {
+  user: User | null;
+  loading: boolean;
+}
+
+export const AuthContext = createContext<AuthContextType>({
+  user: null,
+  loading: true,
+});
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const storedUID = getSessionCookie(); // ✅ Çerezden UID'yi al
+
+    if (storedUID) {
+      setUser({ uid: storedUID } as User); // ✅ Çerezde UID varsa user objesi oluştur
+      setLoading(false);
+      return;
+    }
+    const unsubscribe = onAuthStateChanged(auth, async(currentUser) => {
+    //   setUser(currentUser);
+    //   setLoading(false);
+    if (currentUser) {
+        setSessionCookie(currentUser.uid); 
+        setUser(currentUser);
+      } else {
+        removeSessionCookie(); 
+        setUser(null);
+      }
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, loading}}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
