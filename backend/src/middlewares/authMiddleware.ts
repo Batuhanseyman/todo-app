282b4e19ce375fa957a2816653836c9eba26d2ef
Batuhan SEYMAN <batuhan.seyman@outlook.com>
2025-02-10 17:45:52 +0300

    Update authMiddleWare
    responseModel integration

--- backend/src/middlewares/authMiddleware.ts
+++ backend/src/middlewares/authMiddleware.ts
@@ -1,25 +1,29 @@
 import { Request, Response, NextFunction } from "express";
 import admin from "../configs/firebase/firebaseAdmin";
+import { unauthorized } from "../helpers/responseHelper";
+import { ResponseModel } from "../models/ResponseModel";
 
 const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     try{
         const uid = req.headers.userid as string;
         if(!uid){
-            res.status(401).json({message: "Unauthorized: No UID provided"});
+            const response = new ResponseModel(false, null, "Unauthorized: No UID provided", new Date());
+            unauthorized(res, response);
             return;
         }
 
         const user = await admin.auth().getUser(uid);
         if(!user){
-            res.status(401).json({ message: "Unauthorized: Invalid UID" });
+            const response = new ResponseModel(false, null, "Unauthorized: Invalid UID", new Date());
+            unauthorized(res, response);
             return;
         }
 
         next();
 
     }catch(error){
-        res.status(401).json({ message: "Unauthorized", error });
-
+        const response = new ResponseModel(false, null, "Unauthorized", new Date());
+        unauthorized(res, response);
     }
 };
 
