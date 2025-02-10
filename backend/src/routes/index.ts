2bfa3a3687dde36d2e422df918f4d1af10f63bb3
Batuhan SEYMAN <batuhan.seyman@outlook.com>
2025-02-10 17:55:58 +0300

    Update routues
    added requestCounter route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import todoRoutes from './todoRoutes';
 import userRoutes from './userRoutes'; 
+import requestCounterRoutes  from './requestCounterRoutes'
 
 const router = express.Router();
 
 router.use("/todos", todoRoutes);
 router.use("/register", userRoutes);
+router.use("/requestcounts", requestCounterRoutes);
 
 export default router;
