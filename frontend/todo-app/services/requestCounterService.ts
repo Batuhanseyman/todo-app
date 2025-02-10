b1c84e2c16e0898a1e871e0ebc733ea7b4b9e445
Batuhan SEYMAN <batuhan.seyman@outlook.com>
2025-02-11 01:12:18 +0300

    Update requestCounterService

--- frontend/todo-app/services/requestCounterService.ts
+++ frontend/todo-app/services/requestCounterService.ts
@@ -2,7 +2,17 @@ import { getSessionCookie } from "@/lib/cookies";
 import axios from "axios";
 const API_URL = process.env.NEXT_PUBLIC_BACKEND_API_URI; 
 
-export const getRequestCounts = async () => {
+export interface RequestCounts {
+  DELETE: number;
+  GET: number;
+  POST: number;
+  PUT: number;
+  success: boolean;
+}
+
+
+
+export const getRequestCounts = async (): Promise<RequestCounts | null> => {
     try {
         const uid = getSessionCookie(); 
     
@@ -13,9 +23,12 @@ export const getRequestCounts = async () => {
               "userid": `${uid}`,
             },
           });
-          return {data: response.data.data.counts, success: response.data.success};
+          const requestCounts: RequestCounts = {DELETE: response.data.data.counts.DELETE, GET: response.data.data.counts.GET, 
+            POST: response.data.data.counts.POST, PUT: response.data.data.counts.PUT, success: response.data.success
+          };
+          return requestCounts;
     } catch (error) {
         console.error("Error fetching request counts:", error);
-        return [];
+        return null;
     }
 }
