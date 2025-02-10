17a4bee2ef8641baa53fb91d073be1e4a0939087
Batuhan SEYMAN <batuhan.seyman@outlook.com>
2025-02-10 17:40:20 +0300

    Update todo model
    Added createdAt and updatedAt attributes

--- backend/src/models/Todo.ts
+++ backend/src/models/Todo.ts
@@ -1,16 +1,22 @@
 import mongoose, {Schema, Document} from "mongoose";
 
 export interface ITodo extends Document {
-    userId : string,
-    todo: string,
-    selected: boolean
+    _id: mongoose.Types.ObjectId;
+    userId : string;
+    todo: string;
+    selected: boolean;
+    createdAt?: Date;
+    updatedAt?: Date;
 } 
 
 
-const TodoSchema = new Schema<ITodo>({
+const TodoSchema = new Schema<ITodo>(
+    {
     userId: { type: String, ref: "User", required: true },
     todo: {type : String, required: true},
     selected: { type: Boolean, default: false }
-});
+    },
+    {timestamps: true}
+);
 
 export default mongoose.model<ITodo>("Todo", TodoSchema);
