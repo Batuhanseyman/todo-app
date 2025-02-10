d75e142968f9983ad7e0334360fa0c6e50aebed9
Batuhan SEYMAN <batuhan.seyman@outlook.com>
2025-02-10 17:55:32 +0300

    Update todoRoutes
    requestCounter middleware and controller added

--- backend/src/routes/todoRoutes.ts
+++ backend/src/routes/todoRoutes.ts
@@ -1,13 +1,15 @@
 import express from 'express';
 import * as TodoController from "../controllers/todoController";
 import  authMiddleware  from '../middlewares/authMiddleware';
+import { requestCounterMiddleware } from '../middlewares/requestCounterMiddleware';
 
 const router = express.Router();
 
-router.get( "/:id", authMiddleware, TodoController.getUserTodos);
-router.post( "/", authMiddleware, TodoController.addTodo);
+router.get( "/:id", authMiddleware, requestCounterMiddleware, TodoController.getUserTodos);
+router.post( "/", authMiddleware, requestCounterMiddleware, TodoController.addTodo);
+router.post("/insert-many", authMiddleware, requestCounterMiddleware, TodoController.addLocalTodos)
 
-router.put("/:id", authMiddleware, TodoController.updateUserTodo)
-router.delete("/:id", authMiddleware, TodoController.deleteUserTodo)
+router.put("/:id", authMiddleware, requestCounterMiddleware, TodoController.updateUserTodo)
+router.delete("/:id", authMiddleware, requestCounterMiddleware, TodoController.deleteUserTodo)
 
 export default router;
